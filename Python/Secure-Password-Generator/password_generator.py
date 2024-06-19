3185e352650f55a305f92fcff1c97811203947d5
Joshua Franco <francoswim@yahoo.com>
2024-06-19 11:45:14 -0400

    Generating Random String and password using secrets.choice. This library is used to give a more cryptographically more secure random numbers

--- Python/Secure-Password-Generator/password_generator.py
+++ Python/Secure-Password-Generator/password_generator.py
@@ -6,14 +6,14 @@ Secure Password Generator
 This script generates strong, random passwords based on user defined criteria. It utilizes cryptographic principles to ensure password security
 """
 import string
-import random
+import secrets
 
 DEFAULT_PASSWORD_LENGTH = 12
 
 def generate_password(length = DEFAULT_PASSWORD_LENGTH):
-    
-    random_string = ''.join(random.choices(string.ascii_letters, k=length))
-    print(random_string)
+    random_password = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
+                              for i in range(length))
+    print(random_password)
 
 
 if __name__ == "__main__":
