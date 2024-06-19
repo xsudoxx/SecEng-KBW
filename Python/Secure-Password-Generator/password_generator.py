1b4dd7ece4a2ab4b72ca2ab5796e9d91a149132c
Joshua Franco <francoswim@yahoo.com>
2024-06-19 11:39:10 -0400

    Adding a simple function that print an uppercase password

--- Python/Secure-Password-Generator/password_generator.py
+++ Python/Secure-Password-Generator/password_generator.py
@@ -5,9 +5,15 @@ Secure Password Generator
 
 This script generates strong, random passwords based on user defined criteria. It utilizes cryptographic principles to ensure password security
 """
-def generate_password():
-    pass
+import string
+import random
+
+DEFAULT_PASSWORD_LENGTH = 12
+
+def generate_password(length = DEFAULT_PASSWORD_LENGTH):
+    random_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+    print(random_string)
 
 
 if __name__ == "__main__":
-    print("Function Started")
+    generate_password()
