ecce83f1894b8a78dfd7a35e5aa1b29ba0a5d23a
Joshua Franco <francoswim@yahoo.com>
2024-06-19 11:34:07 -0400

    Adding basic setup of py file for scripts

---
+++ Python/Secure-Password-Generator/password_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Secure Password Generator
+
+This script generates strong, random passwords based on user defined criteria. It utilizes cryptographic principles to ensure password security
+"""
+def generate_password():
+    pass
+
+
+if __name__ == "__main__":
+    print("Function Started")
